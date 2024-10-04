1766d973cd01e10b3b33180a0771ce8131772929
Naveen Siriseti <naveennaviee5@gmail.com>
2024-10-04 12:20:17 +0530

    Add files via upload

---
+++ Select_Query's.sql
@@ -0,0 +1,24 @@
+--Query to  fetch all the tables
+
+select * from INFORMATION_SCHEMA.tables;
+SELECT * FROM sys.TABLES;
+
+select * from Booking_chahel;
+
+select * from Buses_amishi;
+
+select * from Car_chahel;
+
+select * from Cruise_amishi;
+
+select * from CustomerDetails_reneeka;
+
+select * from Destination;
+
+select * from Employee_reneeka;
+
+select * from Flights_amishi;
+
+select * from Payment_chahel;
+
+select * from Trains_reneeka;
